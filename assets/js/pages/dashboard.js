83b0655f8168be99888fc4abc7b3a9206d06a24b
Alexander Ivantsov <shpuntik37@ya.ru>
2015-05-09 13:34:58 +0500

    add title for dashboard

--- assets/js/pages/dashboard.js
+++ assets/js/pages/dashboard.js
@@ -1,23 +1,20 @@
 var React = require('react');
+var DocumentTitle = require('react-document-title');
 var List = require('../components/list');
 
 var Dashboard = React.createClass({
     propTypes: {
         items: React.PropTypes.arrayOf(React.PropTypes.object).isRequired
     },
-    render: function () {
+    render() {
         return (
-            <div className="fluid-container">
-                <div className="jumbotron">
-                    <div className="container">
-                        <h1>Dashboard</h1>
+            <DocumentTitle title="Dashboard">
+                <div className="fluid-container">
+                    <h2>Dashboard</h2>
 
-                        <p>Here you can see all animes</p>
-                    </div>
+                    <List {...this.props}/>
                 </div>
-
-                <List {...this.props}/>
-            </div>
+            </DocumentTitle>
         );
     }
 });
